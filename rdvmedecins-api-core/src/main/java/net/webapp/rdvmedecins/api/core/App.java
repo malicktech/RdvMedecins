d05e0b749d2bc046edf790acd90dd23ad7b94ffd
Malick <diop-malick@hotmail.fr>
2016-03-20 04:53:17 +0100

    init app

---
+++ rdvmedecins-api-core/src/main/java/net/webapp/rdvmedecins/api/core/App.java
@@ -0,0 +1,13 @@
+package net.webapp.rdvmedecins.api.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ rdvmedecins-api-web/src/main/java/net/webapp/api/web/App.java
@@ -0,0 +1,13 @@
+package net.webapp.api.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
